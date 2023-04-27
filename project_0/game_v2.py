95c2064d6527bcca037656ec498967ee5f8a43db
nicktsvetkov <tsvetkov.kolya@gmail.com>
2023-04-27 21:36:24 +0300

    Modify the game

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,24 +1,27 @@
 
 import numpy as np
-def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+def random_predict(number:int=1, low_limit:int=1, up_limit:int=100) -> int:
+    """Ищет число алгоритмом бинарного поиска
 
     Args:
         number (int, optional): Загаданне число, по умолчанию 1. Defaults to 1.
+        low_limit(int, optional): Нижняя граница диапазона поиска. Defaults to 1.
+        up_limit(int, optional):  Верхняя граница диапазона поиска. Defaults to 100.
 
     Returns:
         int: Число попыток
     """
     count = 0
-
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
+        predict_number =  (up_limit + low_limit)//2# предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
+        elif predict_number > number:
+            up_limit = predict_number-1
+        else:
+            low_limit = predict_number+1
     return(count)
-
-print(f'Количество попыток: {random_predict()}')
 
 
 def score_game(random_predict) -> int:
@@ -30,13 +33,14 @@ def score_game(random_predict) -> int:
     Returns:
         int: среднее количество попыток
     """
-
+    low_limit = 1
+    up_limit = 100
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    random_array = np.random.randint(low_limit, up_limit+1, size=(1000)) # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(random_predict(number, low_limit, up_limit))
 
     score = int(np.mean(count_ls)) # находим среднее количество попыток
 
